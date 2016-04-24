93a4aca813a6b822a57914fa7ad136c8afa89259
traveGrobe <traveglobal@gmail.com>
2016-04-24 16:52:52 +0800

    update Product Contructor

--- js/app/dashboard/recomend_product/constructor.js
+++ js/app/dashboard/recomend_product/constructor.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     var arr_dependency, fn_initialize;
@@ -10,7 +10,7 @@
         'util.template'
     ];
 
-    fn_initialize = function(requireCSS, moment, foreach, template) {
+    fn_initialize = function (requireCSS, moment, foreach, template) {
 
         requireCSS([
             'component.product_card'
@@ -18,37 +18,38 @@
 
         var TEMPLATE = {
             item:
-            '<div class="col-md-4">' +
-            '<div class="product_card z-depth-1 hoverable">' +
-            '<div class="card_header view overlay hm-white-slight">' +
-            '<p class="card_label">' +
-            //'<span class="label rgba-black-light">{{ originCountry }}</span>' +
-            '<span class="label label-default"><span class="{{ iconCountry }}">' +
-            '</span>{{ originCountry }}</span>' +
 
-            '</p>' +
-            '<img src="{{ imageSrc }}" class="img-responsive">' +
-            '<div class="mask waves-effect waves-light"> </div>' +
-            '</div>' +
-            '<a class="btn-floating waves-effect waves-light red"><i class="material-icons">add</i></a>' +
-            '<div class="card_content">' +
-            '<p>{{ title }}</p>' +
-            '</div>' +
-            '<div class="card_footer">' +
-            '<p>{{ requestCount }} Request</p>' +
-            '</div>' +
-            '</div>' +
-            '</div>'
-        };
+            <div class="column">
+                <div class="ui fluid card shadow hoverable">
+                    <div class="content">
+                        <a class="ui right corner label">
+                            <i class="star icon"></i>
+                        </a>
+                        <div class="header">Tokyo Banana will the name too long?</div>
+                    </div>
+                    <div class="ui fluid image">
+                        <span class="ui red ribbon label ">
+                            <i class="jp flag"></i>Japan
+                        </span>
+                        <img src="/img/product/tokyo_banana.jpg">
+                </div>
 
-        var fn_constr = (function() {
+                        <div class="extra content center">
+                            <p>1 Request</p>
+                        </div>
+
+                    </div>
+                </div>
+                };
+
+                var fn_constr = (function() {
             var CONFIG_ITEM = 'recomended-item',
                 CONFIG_ON_COMPLETE = 'on-complete';
 
-            return function(obj_config) {
+                return function(obj_config) {
                 var arr_item = obj_config[CONFIG_ITEM],
-                    fn_onComplete = obj_config[CONFIG_ON_COMPLETE],
-                    arr_fragment = [];
+                fn_onComplete = obj_config[CONFIG_ON_COMPLETE],
+                arr_fragment =[];
 
                 foreach(arr_item, function(obj_item) {
                     arr_fragment.push(template(TEMPLATE.item, {
@@ -62,13 +63,13 @@
                 });
 
                 fn_onComplete(arr_fragment.join(''));
-            };
-        } ());
+                };
+                } ());
 
-        return {
-            constr: fn_constr
-        };
-    };
+                return {
+                    constr: fn_constr
+                };
+                };
 
-    define(arr_dependency, fn_initialize);
-} ());
+                define(arr_dependency, fn_initialize);
+                } ());
